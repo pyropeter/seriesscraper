8fd3af74e05b539be9be8304aa015acd7b9305b5
PyroPeter <pyropeter@pyropeter.eu>
2013-07-09 00:25:31 +0200

    gorillavid finished

--- gorillavid_in.py
+++ gorillavid_in.py
@@ -11,12 +11,19 @@ def scrape(url):
 
 def getStreamUrl(url):
 	br = mechanize.Browser(factory=mechanize.RobustFactory())
-	br.open(url)
-
 	# don't do stuff we don't need anyway
 	br.set_handle_refresh(False)
 	br.set_handle_equiv(False)
 	br.set_handle_robots(False)
 
+	# open the first page
+	br.open(url)
+
+	# push the big red button
 	br.select_form(nr=1)
+	br.submit()
+
+	# now it gets ugly
+	html = br.response().read()
+	return re.search(r"file: \"([^\"]+)\"", html).group(1)
 
