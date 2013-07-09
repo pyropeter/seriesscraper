d64f8e523030aa17f921207d66524a2c7477a75e
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-07-09 21:27:58 +0200

    updated to new directory structure

--- test-portals.py
+++ test-portals.py
@@ -1,4 +1,4 @@
-import portals
+import hosters
 
 # this will work as long the video isn't deleted. ah, well...
-assert len(portals.scrape("http://gorillavid.in/ajb5k209eo85").read(1024)) == 1024
+assert len(hosters.stream("http://gorillavid.in/ajb5k209eo85").read(1024)) == 1024
